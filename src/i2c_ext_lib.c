b6a8ba9fe37861544b291d729372981995861b07
Ben Thomas <ben@nhil.net>
2018-03-23 01:19:26 +1100

    try void * cast

--- src/i2c_ext_lib.c
+++ src/i2c_ext_lib.c
@@ -1,13 +1,11 @@
 #include "mgos.h"
 #include "mgos_i2c.h"
-#include "mgos_sys_config.h"
-#include "mgos_config.h"
 
 static struct mgos_i2c *s_global_i2c_ext;
 
 bool mgos_i2c_ext_init(void) {
   if (!mgos_sys_config_get_i2c_ext_enable()) return true;
-  s_global_i2c_ext = mgos_i2c_create((const struct mgos_config_i2c *)mgos_sys_config_get_i2c_ext());
+  s_global_i2c_ext = mgos_i2c_create((void *)mgos_sys_config_get_i2c_ext());
   return (s_global_i2c_ext != NULL);
 }
 
