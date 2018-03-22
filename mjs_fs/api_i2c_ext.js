06432217b2934333e1a62941ee021d79c1dbe560
Ben Thomas <ben@nhil.net>
2018-03-23 00:23:07 +1100

    Fix ffi reference

--- mjs_fs/api_i2c_ext.js
+++ mjs_fs/api_i2c_ext.js
@@ -1,5 +1,5 @@
-let I2CEXTRA = {
-  _int: ffi('bool mgos_i2cextra_init(void)'),
+let I2C_EXT = {
+  _int: ffi('bool mgos_i2c_ext_init(void)'),
 
   // ## **`I2CEXTRA.get()`**
   // Get secondary I2C bus handle. Return value: opaque pointer.
