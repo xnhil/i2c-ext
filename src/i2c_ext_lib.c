d988800ae46e50b3c9e05712d51ecbe007bc6c03
Ben Thomas <ben@nhil.net>
2018-03-23 01:09:13 +1100

    remove dup init and force cast config

--- src/i2c_ext_lib.c
+++ src/i2c_ext_lib.c
@@ -4,16 +4,8 @@
 static struct mgos_i2c *s_global_i2c_ext;
 
 bool mgos_i2c_ext_init(void) {
-  return true;
-}
-
-bool mgos_i2c_ext_init(void) {
   if (!mgos_sys_config_get_i2c_ext_enable()) return true;
-  // should do sanity checks
-  // supported system e.g. ESP32 h/w or GPIO s/w driven
-  // ESP32: Flip unit in configs, pins are separate
-  // GPIO: check pins sep
-  s_global_i2c_ext = mgos_i2c_create(mgos_sys_config_get_i2c_ext());
+  s_global_i2c_ext = mgos_i2c_create((const mgos_config_i2c *)mgos_sys_config_get_i2c_ext());
   return (s_global_i2c_ext != NULL);
 }
 
